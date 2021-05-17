8507f6afb88b7178ec56ae10bc61b5c02f5e9342
Erika Antal <Erika.timea.ea@gmail.com>
2021-05-17 23:31:10 +0300

    Uploaded Photos in images folder, changed path for picturebox.

--- FinalProjectCurs/Form1.cs
+++ FinalProjectCurs/Form1.cs
@@ -30,40 +30,40 @@ namespace FinalProjectCurs
             switch (comboBoxMasina.SelectedItem.ToString())
             {
                 case "AudiA3":
-                    pictureBox_car.Load("pictures\\3.png");
-                    pictureBox_car1.Load("pictures\\3.png");
-                    pictureBox_car2.Load("pictures\\33.png");
-                    pictureBox_car3.Load("pictures\\333.png");
+                    pictureBox_car.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\3.png");
+                    pictureBox_car1.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\3.png");
+                    pictureBox_car2.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\33.png");
+                    pictureBox_car3.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\333.png");
                     break;
                 case "VolkswagenTiguan":
-                    pictureBox_car.Load("pictures\\4.png");
-                    pictureBox_car1.Load("pictures\\4.png");
-                    pictureBox_car2.Load("pictures\\44.png");
-                    pictureBox_car3.Load("pictures\\444.jpg");
+                    pictureBox_car.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\4.png");
+                    pictureBox_car1.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\4.png");
+                    pictureBox_car2.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\44.png");
+                    pictureBox_car3.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\444.jpg");
                     break;
                 case "BMWI8":
-                    pictureBox_car.Load("pictures\\2.png");
-                    pictureBox_car1.Load("pictures\\2.png");
-                    pictureBox_car2.Load("pictures\\22.jpg");
-                    pictureBox_car3.Load("pictures\\222.png");
+                    pictureBox_car.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\2.png");
+                    pictureBox_car1.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\2.png");
+                    pictureBox_car2.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\22.jpg");
+                    pictureBox_car3.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\222.png");
                     break;
                 case "HyundaiTucson":
-                    pictureBox_car.Load("pictures\\5.png");
-                    pictureBox_car1.Load("pictures\\5.png");
-                    pictureBox_car2.Load("pictures\\55.png");
-                    pictureBox_car3.Load("pictures\\555.jpg");
+                    pictureBox_car.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\5.png");
+                    pictureBox_car1.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\5.png");
+                    pictureBox_car2.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\55.png");
+                    pictureBox_car3.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\555.jpg");
                     break;
                 case "LamborghiniAventador":
-                    pictureBox_car.Load("pictures\\1.png");
-                    pictureBox_car1.Load("pictures\\1.png");
-                    pictureBox_car2.Load("pictures\\11.png");
-                    pictureBox_car3.Load("pictures\\111.png");
+                    pictureBox_car.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\1.png");
+                    pictureBox_car1.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\1.png");
+                    pictureBox_car2.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\11.png");
+                    pictureBox_car3.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\111.png");
                     break;
                 case "RenaulClio":
-                    pictureBox_car.Load("pictures\\6.jpg");
-                    pictureBox_car1.Load("pictures\\6.jpg");
-                    pictureBox_car2.Load("pictures\\66.png");
-                    pictureBox_car3.Load("pictures\\666.jpg");
+                    pictureBox_car.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\6.jpg");
+                    pictureBox_car1.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\6.jpg");
+                    pictureBox_car2.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\66.png");
+                    pictureBox_car3.Image = Image.FromFile("C:\\Users\\AE\\Desktop\\CatalinRadu11\\FinalProjectCurs\\FinalProjectCurs\\images\\666.jpg");
                     break;
                 default:
                     MessageBox.Show("Nu ai ales nicio masina!"); //nu functioneaza inca
@@ -72,17 +72,17 @@ namespace FinalProjectCurs
         }
         private void pictureBox_car1_Click(object sender, EventArgs e)
         {
-            pictureBox_car.Image = pictureBox_car1.Image;
+            pictureBox_car.Image = Image.FromFile(@"C:\Users\AE\Desktop\CatalinRadu11\FinalProjectCurs\FinalProjectCurs\images");
         }
 
         private void pictureBox_car2_Click(object sender, EventArgs e)
         {
-            pictureBox_car.Image = pictureBox_car2.Image;
+            pictureBox_car.Image = Image.FromFile(@"C:\Users\AE\Desktop\CatalinRadu11\FinalProjectCurs\FinalProjectCurs\images");
         }
 
         private void pictureBox_car3_Click(object sender, EventArgs e)
         {
-            pictureBox_car.Image = pictureBox_car3.Image;
+            pictureBox_car.Image = Image.FromFile(@"C:\Users\AE\Desktop\CatalinRadu11\FinalProjectCurs\FinalProjectCurs\images");
         }
         private void buttonRezervaAcum_Click_1(object sender, EventArgs e)
         {
